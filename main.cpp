e8cee22c6421c94358c426363590e3b99c249f03
colliderman <cody@empresseffects.com>
2020-02-22 13:42:47 +0000

    Completed 10 UDT's

--- main.cpp
+++ main.cpp
@@ -53,42 +53,152 @@ struct CarWash            //1) a U.D.T. with a random number of member variables
 /*
  1)
  */
+struct Timer
+{
+    bool startButtonPressed = false;
+    bool startButtonHeld = false;
+
+    struct Time
+    {
+        unsigned int seconds, minutes, hours;
+    };
+
+    void startTimer( Time* time );
+    void stopTimer( Time* time );
+    void resetTimer( Time* time );
+};
 
 /*
  2)
  */
+struct Dog
+{
+    char* name, breed;
+    bool isGoodBoy = true;
+    int numBallsFetched = 0;
+
+    void chaseCar( CarWash::Car carToChase );
+
+    int fetchBall() { return numBallsFetched; }
+
+    bool checkIfGoodBoy( int numBallsFetched );
+};
 
 /*
  3)
  */
+struct DrinkBeer
+{
+    unsigned int numBeersDrank;
+    bool isDrunk = false;
+
+    struct Beer
+    {
+        bool isLager;
+        float alcoholPercentage;
+    };
+
+    unsigned int drinkBeer( Beer beer );
+
+    bool checkIfDrunk ( unsigned int numOfBeers );
+
+    Beer stout, maltLiquor, lightBeer;
+};
 
 /*
  4)
  */
+struct Tree
+{
+    int numBranches;
+    float height;
+
+    void climbTree( float howHigh );
+    
+}; 
 
 /*
  5)
  */
+struct GuitarPedal
+{
+    char* nameOfPedal;
+    int numOfKnobs;
+    bool isModulation;
+
+    void setPedalType( int pedalType );
+    char* getPedalName( GuitarPedal pedal );
+
+
+};
 
 /*
  6)
  */
+struct DelayPedal
+{
+    const int pedalType = 1;
+    double delayTime, mix, feedback;
+    bool hasTapTempo = true;
+
+    void setTapTempo( int multiplier );
+    void setDelaytime();
+};
 
 /*
  7)
  */
+struct TremoloPedal
+{
+    const int pedalType = 2;
+    double rate, rhythm, waveform;
+    bool hasTapTempo = true;
+
+    void setTapTempo( int multiplier );
+    void setTremoloRate();
+};
 
 /*
  8)
  */
+struct PatchCable
+{
+    int cableLength;
+    bool isRightAngle;
+
+    int getCableLength( PatchCable cable);
+    int setCableColour( int colour );
+};
+
 
 /*
  9)
  */
+struct PowerSupply
+{
+    float voltage;
+    int maxCurrentOutput;
+    bool hasTransformer = false;
+
+    void setPowerSupplySpecs( PowerSupply powerSupply );
+
+};
+
 
 /*
  10)
  */
+ struct PedalBoard
+{
+    GuitarPedal pedal1;
+    GuitarPedal pedal2;
+    GuitarPedal pedal3;
+    PatchCable  patchCable;
+    PowerSupply powerSupply;
+
+    void setPedalOrder( PedalBoard pedalBoard );
+
+};
 
 #include <iostream>
 int main()
