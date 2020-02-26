1c1af98027168802c7ffb9421e491b8ad0fb650a
matkatmusic <matkatmusic@gmail.com>
2020-02-25 18:33:53 -0800

    added part3 instructions

--- main.cpp
+++ main.cpp
@@ -1,98 +1,56 @@
- #include <iostream>
+/*
+ Project 3 - Part 3 / 5
+ video: Chapter 2 - Part 8
+ Constructors tasks
 
- /*
- Project 3 - Part 2 / 5
- Video: Chapter 2 Part 6
- Implementations tasks
+ Create a branch named Part3
 
-Create a branch named Part2
+ On a new branch:
 
- tasks
- 1) write the definition for the Type that leftFoot and rightFoot are instantiations of.
-    don't forget to define and implement the member functions 'stepForward()' and 'stepSize()'
-    you should be able to deduce the return type of those functions based on their usage in Person::run()
-    You'll need to insert the Person struct from the video in the space below.
- */
-struct Person
-{
-    int age;
-    int height;
-    float hairLength;
-    float GPA;
-    unsigned int SATScore;
-    int distanceTravelled;
-
-    void run( int howFast, bool startWithLeftFoot);
-
-    struct foot
-    {
-        bool didStepForward;
-
-        void stepForward()
-        {
-            didStepForward = true;
-        }
-        int stepSize()
-        {
-            //inches
-            int footSize = 9;
-            int averageHeight = 69; 
-            int stepSize = (averageHeight/5) + footSize; //just making this up!
-
-            return stepSize;
-        }
-        
-    };
-
-    foot leftFoot, rightFoot;
-};
-
-void Person::run( int howFast, bool startWithLeftFoot )
-{
-    if( startWithLeftFoot )
-    {
-        leftFoot.stepForward();
-        rightFoot.stepForward();
-    }
-    else
-    {
-        rightFoot.stepForward();
-        leftFoot.stepForward();
-    }
-    distanceTravelled += leftFoot.stepSize() + rightFoot.stepSize() + howFast;
-}
- /*
- 2) provide implementations for the member functions you declared in your 10 user-defined types from the previous video outside of your UDT definitions.
+ 1) Add a constructor for each User-Defined-Type.
  
- 3) be sure to write the correct full qualified name for the nested type's member functions.
+ 2) Instantiate 1 or 2 instances of each of your user-defined types in the main function at the bottom of that file, and call some of those member functions.
  
- 4) After you finish defining each type/function, click the [run] button.  Clear up any errors or warnings as best you can.
+ 3) make them print out something interesting via std::cout
+ 
+ After you finish defining each type/function:
+ click the [run] button.  Clear up any errors or warnings as best you can.
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
-Make a pull request after you make your first commit and pin the pull request link to our DM thread.
+ Make a pull request after you make your first commit and pin the pull request link to our DM thread.
 
 send me a DM to check your pull request
 
  Wait for my code review.
+ 
+ example:
  */
 
-struct CarWash            //1) a U.D.T. with a random number of member variables
+#include <iostream>
+namespace Example 
 {
-    int numSponges = 3;
-    double amountOfSoapUsedPerCar = 2.6; //2) relevant data types
-    unsigned int numCarsProcessed = 0;
-    
-    struct Car                             //4) nested class
+struct UDT  // my user defined type
+{
+    int a; //a member variable
+    UDT() { a = 0; }             //3) the constructor
+    void printThing()            //1) the member function
     {
-        bool isAPickupTruck = false;
-        float gasMileage = 26.2f;        //2) relevant data types
-    };
-    
-    void washAndWaxCar( Car car );         //3) member function with a user-defined type as the parameter.  The user-defined type parameter happens to be the nested class.
-    
-    Car myCar;  //5) a member variable whose type is a UDT.
+        std::cout << "UDT::printThing() " << a << std::endl;  //5) printing out something interesting
+    }
 };
+
+int main()
+{
+    UDT foo;              //4) instantiating a UDT in main()
+    foo.printThing();     //4) calling a member function of the instance that was instantiated.
+    
+    return 0;
+}
+}
+
+//insert Example::main() into main() of user's repo.
+
 
 #include <string>
 
@@ -409,5 +367,6 @@ void PedalBoard::setPedalOrder()
 
 int main()
 {
+    Example::main();
     std::cout << "good to go!" << std::endl;
 }
